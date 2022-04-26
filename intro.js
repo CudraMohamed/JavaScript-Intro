fa2f87fa4f10d98dfc0b147e5a3c6e2e09308f83
CudraMohamed <ncudramohamed@gmail.com>
2022-04-26 23:35:44 +0300

    part 1 changes

---
+++ intro.js
@@ -0,0 +1,30 @@
+var a=5+5 //number
+console.log(a)
+//a="Cudra"
+const b='Cudra' //string
+var c=`Mohamed`  //string
+//b="Douglas"
+console.log(typeof b)
+console.log(typeof a)
+console.log(a,b,c)
+var d=true  //boolean
+console.log(typeof d)
+var e =["word",1,true]
+console.log(e)
+console.log(typeof e)
+var f=[1,2,3,false,e]
+console.log(f)
+var g={school:"AkiraChix","primary school":"Primary"}
+console.log(g)
+console.log(g["primary school"])  //accessing objects
+console.log(g.school)  //accessing objects
+g["school"]="KU"  //changing values
+g.school="MT.K"   //changing values
+g["workplace"]="iHub"  //setting new values
+console.log(g)
+g.javascript=true
+console.log(g)
+g["home/address"]="Ngong"
+console.log(g)
+g["a"]=a
+console.log(g)
